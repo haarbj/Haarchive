f990f54216b57630526ca022761bc17fcfe6efa8
Brody Haar <brody.j.haar@vanderbilt.edu>
2026-07-06 16:02:15 -0700

    Fix basePath casing

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/haarchive" : "",
-  assetPrefix: isProd ? "/haarchive/" : "",
+  basePath: isProd ? "/Haarchive" : "",
+  assetPrefix: isProd ? "/Haarchive/" : "",
   images: {
     unoptimized: true,
   },
